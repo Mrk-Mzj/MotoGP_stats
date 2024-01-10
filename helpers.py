3ef61c1e43d8e9aca57f92fce1ff9158aa158849
Mrk-Mzj <Marek.Mazij@gmail.com>
2024-01-10 02:03:52 +0100

    feat: drawing line graphs

--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
+from io import StringIO
 import requests
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
@@ -29,7 +31,7 @@ class ScrappingReasultsFrom:
             sup.extract()
 
         # create list of all tables (dataframes):
-        df_tables = pd.read_html(str(soup), attrs={"class": "wikitable"})
+        df_tables = pd.read_html(StringIO(str(soup)), attrs={"class": "wikitable"})
 
         # extract riders standings table:
         df_riders = pd.DataFrame()
@@ -46,12 +48,12 @@ class ScrappingReasultsFrom:
         return df_riders
 
     # scrapping teams standings #TODO
-    def teams(self):
+    def teams(self) -> pd.DataFrame:
         ...
 
     # scrapping constructors standings #TODO
-    # def constructors(self):
-    #     ...
+    def constructors(self) -> pd.DataFrame:
+        ...
 
 
 # class Weather: #TODO
@@ -60,26 +62,60 @@ class ScrappingReasultsFrom:
 
 class Cleaning:
     def __new__(cls, df: pd.DataFrame) -> pd.DataFrame:
-        # removing columns:
-        df.drop(columns=["Bike", "Team", "Pts"], inplace=True)
+        # removing columns
+        df.drop(columns=["Pos.", "Bike", "Team", "Pts"], inplace=True)
 
-        # removing last two rows:
+        # removing last two rows
         df.drop(df.tail(2).index, inplace=True)
 
-        # converting status strings to 0:
+        # converting status strings to 0
         df.replace(["Ret", "DNS", "NC", "WD", "DNQ"], 0, inplace=True)
 
         # converting NaN (empty cells) to 0:
         df.fillna(0, inplace=True)
 
+        # setting index to rider name
+        df.set_index("Rider", inplace=True)
+
+        # converting columns from object to numeric:
+        df[df.columns] = df[df.columns].apply(pd.to_numeric)
+
         return df
 
 
 class Plotting:
-    def __init__(self, df: pd.DataFrame):
-        self.df = df
-        # df.plot()
-        # plt.show()
+    def __new__(cls, df: pd.DataFrame, limit_drivers=0, limit_races=0) -> None:
+        cls.df = df
+
+        # limit to n drivers
+        if limit_drivers:
+            df.drop(index=df.index[limit_drivers:], inplace=True)
+
+        # limit to n races
+        if limit_races:
+            df.drop(columns=df.columns[limit_races:], inplace=True)
+
+        print(df, "\n")
+
+        # setting plot size in pixels / dpi
+        plt.figure(figsize=(900 / 72, 400 / 72))
+
+        # plot drivers with Matplotlib:
+        for rider in df.index:
+            plt.plot(df.columns, df.loc[rider], marker="o", label=rider)
+
+        # plot drivers - other method:
+        # for index, row in df.iterrows():
+        #     plt.plot(row, marker="o", label=index)
+
+        plt.title("Riders' standings")
+        plt.legend()
+        plt.xlabel("Races")
+        plt.xticks(rotation=45)
+        plt.ylabel("Place")
+
+        # show plot
+        plt.show()  # TODO: (1)flip plot upside down, (2)convert 0 to 30 / break
 
 
 def main():
@@ -87,9 +123,8 @@ def main():
     Cleaning(results)
 
     print()
-    print(results)  # results.to_string() to show all
-
-    # Plotting(results.riders()) #TODO
+    # print(results.to_json()) # TODO: saving to cache
+    Plotting(results, limit_drivers=5)
 
 
 if __name__ == "__main__":
