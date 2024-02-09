892542ae036e8a33a63b185f620524d7fdafa678
Mrk-Mzj <Marek.Mazij@gmail.com>
2024-02-09 04:08:19 +0100

    feat: setting range of riders to show

--- charts_app/utils/MotoGP_utils.py
+++ charts_app/utils/MotoGP_utils.py
@@ -513,17 +513,16 @@ class Plotting:
         # plt.show()
 
 
-def plot_chart(year=2023, show_average_hist_results = False):
-    # config:
-    show_riders_pos = [1, 5]
+def plot_chart(year=2023, show_average_hist_results=False, show_riders_pos=[1, 5]):
+
     MIN_YEAR = 2004  # earlier data are corrupted
 
     if year < MIN_YEAR:
         raise ValueError("Year must be >= 2004")
-    
+
     # do not plot historic data out of safe range
-    if year < MIN_YEAR+3:
-        show_average_hist_results = False 
+    if year < MIN_YEAR + 3:
+        show_average_hist_results = False
 
     # gathering weather data
     weather = GatheringReasultsFrom(year).weather()
--- charts_app/views.py
+++ charts_app/views.py
@@ -9,17 +9,39 @@ CURRENT_YEAR = datetime.now().year
 MIN_YEAR = 2004  # earlier data are corrupted
 
 
-class parameters_form(forms.Form):
+class ParametersForm(forms.Form):
+
+    # year
     years_list = [tuple([x, x]) for x in range(MIN_YEAR, CURRENT_YEAR + 1)]
 
     year_chosen = forms.IntegerField(
-        label="Select year", widget=forms.Select(choices=years_list)
+        label="Select year", widget=forms.Select(choices=years_list), initial=CURRENT_YEAR
     )
 
+    # checkbox
     hist_results = forms.BooleanField(
         label="Show average prev. 3 years results",
         widget=forms.CheckboxInput(attrs={"id": "checkbox"}),
         required=False,
+    )
+
+    # riders to show
+    places_from = forms.IntegerField(
+        label="Show places from",
+        min_value=1,
+        max_value=20,
+        widget=forms.NumberInput(attrs={"id": "show-from"}),
+        required=True,
+        initial=1,
+    )
+
+    places_to = forms.IntegerField(
+        label="– to place number",
+        min_value=1,
+        max_value=20,
+        widget=forms.NumberInput(attrs={"id": "show-to"}),
+        required=True,
+        initial=5,
     )
 
 
@@ -34,8 +56,28 @@ def index(request):
         # if checkbox is "on", set True, otherwise False
         show_average_hist_results = request.POST.get("hist_results", False)
 
+
+        # checking if user really filled the form
+        if request.POST.get("places_from"):
+            places_from = int(request.POST.get("places_from"))
+        else:
+            places_from = 1
+
+        if request.POST.get("places_to"):
+            places_to = int(request.POST.get("places_to"))
+        else:
+            places_to = 1
+
+
+        # checking if user didn't mix the values
+        if places_from <= places_to:
+            show_riders_pos = [places_from, places_to]
+        else:
+            show_riders_pos = [places_to, places_from]
+
+
         if year in range(MIN_YEAR, CURRENT_YEAR + 1):
-            plot_chart(year, show_average_hist_results)
+            plot_chart(year, show_average_hist_results, show_riders_pos)
 
             # render and fill form with entered data
             return render(
@@ -43,7 +85,7 @@ def index(request):
                 "charts_app/index.html",
                 {
                     "MEDIA_URL": settings.MEDIA_URL,
-                    "form": parameters_form(request.POST),
+                    "form": ParametersForm(request.POST),
                 },
             )
 
@@ -52,5 +94,5 @@ def index(request):
         return render(
             request,
             "charts_app/index.html",
-            {"MEDIA_URL": settings.MEDIA_URL, "form": parameters_form()},
+            {"MEDIA_URL": settings.MEDIA_URL, "form": ParametersForm()},
         )
